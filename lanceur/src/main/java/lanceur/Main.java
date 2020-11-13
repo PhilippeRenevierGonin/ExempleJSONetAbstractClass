73c069bd303a5b54cc18d52c499911135b77c29a
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2020-11-13 19:14:21 +0100

    mise à jour du lanceur

--- lanceur/src/main/java/lanceur/Main.java
+++ lanceur/src/main/java/lanceur/Main.java
@@ -6,11 +6,15 @@ import moteur.Serveur;
 public class Main {
 
     public static void main(String [] args) {
-        Client client = new Client("DémoPlayer", 1);
+        Client client1 = new Client("DémoPlayer", 1);
+        Client client2 = new Client("OtherDémoPlayer", 1);
         Serveur serveur = new Serveur("127.0.0.1",10101);
 
-        serveur.démarrer();
-        client.rejoindreUnePartie();
+        // d'abord les clients car lancerPartie est bloquant
+        client1.rejoindreUnePartie();
+        client2.rejoindreUnePartie();
 
+        serveur.démarrer();
+        serveur.lancerPartie();
     }
 }
